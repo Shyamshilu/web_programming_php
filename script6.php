82602e8b2f8c26a334872c229bb079c126d5e7d3
Shyamshilu <shyamshilu77@gmail.com>
2024-09-04 10:55:45 +0530

    Add files via upload

---
+++ script6.php
@@ -0,0 +1,14 @@
+<html>
+<head></head>
+<body>
+	<?php
+		$heading="Welcome";
+		$bold="This is bold text";
+		$italic="This is italic text";
+		?>
+		
+		<h1><?php echo $heading;?></h1>
+		<p><b> <?php echo $bold ; ?></b><br></p>
+		<p><i> <?php echo $italic ; ?></i></p>
+</body>
+</html>
